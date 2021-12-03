1ae54fc168e23aeb6eb139c552809be06e3f9f60
Adam Ivy <ivya@flip1.engr.oregonstate.edu>
2021-12-03 12:59:51 -0800

    CREATE functionality finished

--- website/webapp.py
+++ website/webapp.py
@@ -11,20 +11,19 @@ webapp = Flask(__name__)
 # Capture information for the sidebar
 def get_sidebar_recipe_details() -> []:
     db_connection = connect_to_database()
-    recipeTypeQuery = "SELECT recipeTypeID, recipeTypeName FROM recipetype;"
+    recipeTypeQuery = "SELECT recipeTypeID, recipeTypeName FROM recipetype ORDER BY recipeTypeName;"
     recipeTypes = execute_query(db_connection, recipeTypeQuery).fetchall()
-    cuisineTypeQuery = "SELECT cuisineTypeID, cuisineTypeName FROM cuisinetype;"
+    cuisineTypeQuery = "SELECT cuisineTypeID, cuisineTypeName FROM cuisinetype ORDER BY cuisineTypeName;"
     cuisineTypes = execute_query(db_connection, cuisineTypeQuery).fetchall()
-    avoidTypeQuery = "SELECT restrictionID, restrictionName FROM foodstoavoid;"
+    avoidTypeQuery = "SELECT restrictionID, restrictionName FROM foodstoavoid ORDER BY restrictionName;"
     avoidTypes = execute_query(db_connection, avoidTypeQuery).fetchall()
     return [recipeTypes, cuisineTypes, avoidTypes]
 
 # Check if a recipe already exists in the database
 def does_recipe_exist(recipeURL) -> []:
     db_connection = connect_to_database()
-    existQuery = "SELECT recipeID, isDeleted FROM recipes where url = '%s';"
-    data = (recipeURL)
-    result = execute_query(db_connection, existQuery, data).fetchone()
+    existQuery = "SELECT recipeID, isDeleted FROM recipes where url = '" + recipeURL + "';"
+    result = execute_query(db_connection, existQuery).fetchone()
     if result == None:
         recipeFound = False
         isDeleted = False
@@ -49,9 +48,25 @@ def add_recipe(recipeName, recipeURL, recipeType, cuisineType):
 # Undelete a recipe
 def undelete_recipe(recipeURL):
     db_connection = connect_to_database()
-    updateQuery = "UPDATE recipes SET isDeleted = 0 WHERE url = %s;"
-    data = (recipeURL)
-    execute_query(db_connection, updateQuery, data)
+    print(recipeURL)
+    updateQuery = "UPDATE recipes SET isDeleted = 0 WHERE url = '" + recipeURL + "';"
+    execute_query(db_connection, updateQuery)
+    return
+
+# Add recipe restrictions
+def add_recipe_restrictions(recipeURL, recipeRestrictions):
+    db_connection = connect_to_database()
+    if len(recipeRestrictions) > 0:
+        getIdQuery = "SELECT recipeID FROM recipes WHERE url = '" + recipeURL + "';"
+        recipeID = execute_query(db_connection, getIdQuery).fetchone()
+        insertQuery = "INSERT INTO reciperestrictions (recipeID, restrictionID) values "
+        for restriction in recipeRestrictions:
+            print("recipeID = " + str(recipeID[0]) + ", restrictionID = " + str(restriction))
+            insertQuery = insertQuery + "(" + str(recipeID[0]) + ", " + str(restriction) + "), "
+        insertQuery = insertQuery[:-2]
+        insertQuery += ";"
+        print(insertQuery)
+        execute_query(db_connection, insertQuery)
     return
 
 # Handler for homepage
@@ -95,6 +110,7 @@ def addRecipe():
         if existing[1]:
             # If it's been deleted, reactivate it.
             undelete_recipe(recipeURL)
+            print('Recipe undeleted.')
         else:
             # Recipe already exists and it's not deleted.
             print('Recipe already present.')
@@ -103,73 +119,10 @@ def addRecipe():
         recipeName = request.form['recipeName']
         recipeType = request.form['type']
         cuisineType = request.form['cuisine']
+        recipeRestrictions = request.form.getlist('avoid')
+        print(request.form)
+        print(recipeRestrictions)
         add_recipe(recipeName, recipeURL, recipeType, cuisineType)
-    flash("Recipe added successfully!")
+        add_recipe_restrictions(recipeURL, recipeRestrictions)
+    # flash("Recipe added successfully!")
     return redirect('/')
-
-# @webapp.route('/home')
-# def home():
-#     db_connection = connect_to_database()
-#     query = "DROP TABLE IF EXISTS diagnostic;"
-#     execute_query(db_connection, query)
-#     query = "CREATE TABLE diagnostic(id INT PRIMARY KEY, text VARCHAR(255) NOT NULL);"
-#     execute_query(db_connection, query)
-#     query = "INSERT INTO diagnostic (text) VALUES ('MySQL is working');"
-#     execute_query(db_connection, query)
-#     query = "SELECT * from diagnostic;"
-#     result = execute_query(db_connection, query)
-#     for r in result:
-#         print(f"{r[0]}, {r[1]}")
-#     return render_template('home.html', result = result)
-
-# @webapp.route('/db_test')
-# def test_database_connection():
-#     print("Executing a sample query on the database using the credentials from db_credentials.py")
-#     db_connection = connect_to_database()
-#     query = "SELECT * from bsg_people;"
-#     result = execute_query(db_connection, query)
-#     return render_template('db_test.html', rows=result)
-
-# #display update form and process any updates, using the same function
-# @webapp.route('/update_people/<int:id>', methods=['POST','GET'])
-# def update_people(id):
-#     print('In the function')
-#     db_connection = connect_to_database()
-#     #display existing data
-#     if request.method == 'GET':
-#         print('The GET request')
-#         people_query = 'SELECT character_id, fname, lname, homeworld, age from bsg_people WHERE character_id = %s'  % (id)
-#         people_result = execute_query(db_connection, people_query).fetchone()
-
-#         if people_result == None:
-#             return "No such person found!"
-
-#         planets_query = 'SELECT planet_id, name from bsg_planets'
-#         planets_results = execute_query(db_connection, planets_query).fetchall()
-
-#         print('Returning')
-#         return render_template('people_update.html', planets = planets_results, person = people_result)
-#     elif request.method == 'POST':
-#         print('The POST request')
-#         character_id = request.form['character_id']
-#         fname = request.form['fname']
-#         lname = request.form['lname']
-#         age = request.form['age']
-#         homeworld = request.form['homeworld']
-
-#         query = "UPDATE bsg_people SET fname = %s, lname = %s, age = %s, homeworld = %s WHERE character_id = %s"
-#         data = (fname, lname, age, homeworld, character_id)
-#         result = execute_query(db_connection, query, data)
-#         print(str(result.rowcount) + " row(s) updated")
-
-#         return redirect('/browse_bsg_people')
-
-# @webapp.route('/delete_people/<int:id>')
-# def delete_people(id):
-#     '''deletes a person with the given id'''
-#     db_connection = connect_to_database()
-#     query = "DELETE FROM bsg_people WHERE character_id = %s"
-#     data = (id,)
-
-#     result = execute_query(db_connection, query, data)
-#     return (str(result.rowcount) + "row deleted")
