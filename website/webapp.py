df4c49bd0e78fbb9ef2f4d38a2cc32a83694c2b5
Adam Ivy <ivya@flip1.engr.oregonstate.edu>
2021-12-03 22:23:16 -0800

    Updated UI

--- website/webapp.py
+++ website/webapp.py
@@ -8,6 +8,59 @@ import requests
 # Create the web application
 webapp = Flask(__name__)
 
+# Get recipe details
+def get_recipe_details(recipeURL):
+    newUrl = recipeURL
+    reqRecipeJSON = {'URL': newUrl}
+    recipeData = requests.post('https://cs361recipescraper.herokuapp.com/scrape', json=reqRecipeJSON)
+    parsedRecipe = json.loads(recipeData.text)
+    return parsedRecipe
+
+# Query the random recipe generator service
+def query_random_service(arraySize):
+    randomReqJSON = {"random_array": {"array_length": arraySize}}
+    reqData = requests.post('https://cs360-random-recipe-generator.herokuapp.com/', json=randomReqJSON)
+    parsedResponse = json.loads(reqData.text)
+    returnIndex = parsedResponse['random_array']['return_index']
+    return returnIndex
+
+# Create types_list
+def create_types_list(types):
+    if len(types) > 0:
+        typeQuery = "in ("
+        for typeID in types:
+            typeQuery = typeQuery + str(typeID) + ", "
+        typeQuery = typeQuery[:-2]
+        typeQuery += ") "
+    else:
+        typeQuery = ""
+    return typeQuery
+
+# Get array of recipes from the database
+def get_recipe_array(recipeTypes, cuisineTypes, avoidTypes) -> []:
+    db_connection = connect_to_database()
+    recipeQuery = create_types_list(recipeTypes)
+    if len(recipeQuery) > 0:
+        recipeQuery = "recipeType " + recipeQuery
+        addAnd = True
+    cuisineQuery = create_types_list(cuisineTypes)
+    if len(cuisineQuery) > 0:
+        cuisineQuery = "cuisineType " + cuisineQuery
+        if addAnd:
+            cuisineQuery = " AND " + cuisineQuery
+        addAnd = True
+    avoidQuery = create_types_list(avoidTypes)
+    if len(avoidQuery) > 0:
+        avoidQuery = "recipeID NOT IN (SELECT recipeID FROM reciperestrictions WHERE restrictionID " + avoidQuery + ")"
+        if addAnd:
+            avoidQuery = " AND " + avoidQuery
+    if(len(recipeQuery) > 0 or len(cuisineQuery) > 0 or len(avoidQuery) > 0):
+        selectQuery = "SELECT url FROM recipes WHERE " + recipeQuery + cuisineQuery + avoidQuery + ";"
+    else:
+        selectQuery = "SELECT url FROM recipes;"
+    recipeIdList = execute_query(db_connection, selectQuery).fetchall()
+    return recipeIdList
+
 # Capture information for the sidebar
 def get_sidebar_recipe_details() -> []:
     db_connection = connect_to_database()
@@ -48,7 +101,6 @@ def add_recipe(recipeName, recipeURL, recipeType, cuisineType):
 # Undelete a recipe
 def undelete_recipe(recipeURL):
     db_connection = connect_to_database()
-    print(recipeURL)
     updateQuery = "UPDATE recipes SET isDeleted = 0 WHERE url = '" + recipeURL + "';"
     execute_query(db_connection, updateQuery)
     return
@@ -61,11 +113,9 @@ def add_recipe_restrictions(recipeURL, recipeRestrictions):
         recipeID = execute_query(db_connection, getIdQuery).fetchone()
         insertQuery = "INSERT INTO reciperestrictions (recipeID, restrictionID) values "
         for restriction in recipeRestrictions:
-            print("recipeID = " + str(recipeID[0]) + ", restrictionID = " + str(restriction))
             insertQuery = insertQuery + "(" + str(recipeID[0]) + ", " + str(restriction) + "), "
         insertQuery = insertQuery[:-2]
         insertQuery += ";"
-        print(insertQuery)
         execute_query(db_connection, insertQuery)
     return
 
@@ -81,14 +131,11 @@ def index():
 # Handler to retrieve recipe
 @webapp.route('/get_recipe', methods=['POST'])
 def getRecipe():
-    newUrl = request.form['url']
-    reqRecipeJSON = {'URL': newUrl}
-    recipeData = requests.post('https://cs361recipescraper.herokuapp.com/scrape', json=reqRecipeJSON)
-    parsedRecipe = json.loads(recipeData.text)
+    parsedRecipe = get_recipe_details(request.form['url'])
     db_details = get_sidebar_recipe_details()
     recipeTypes = db_details[0]
     cuisineTypes = db_details[1]
-    avoidTypes = db_details[2]   
+    avoidTypes = db_details[2]  
     return render_template('import_recipe.html', 
         imageURL = parsedRecipe['recipe']['image_url'],
         name = parsedRecipe['recipe']['name'],
@@ -192,3 +239,38 @@ def deleteRestrictionType():
     deleteQuery = "DELETE FROM foodstoavoid WHERE restrictionID = " + str(request.form['restrictionID']) + ";"
     execute_query(db_connection, deleteQuery)
     return redirect('/settings')
+
+# Handler for generating random recipe
+@webapp.route('/generate_random', methods=['POST'])
+def generateRandom():
+    db_connection = connect_to_database()
+    recipeTypes = request.form.getlist('recipeTypes')
+    cuisineTypes = request.form.getlist('cuisineTypes')
+    avoidTypes = request.form.getlist('avoidTypes')
+    recipeList = get_recipe_array(recipeTypes, cuisineTypes, avoidTypes)
+    recipeIndex = query_random_service(len(recipeList))
+    outputRecipe = recipeList[recipeIndex][0]
+    parsedRecipe = get_recipe_details(outputRecipe)
+    db_details = get_sidebar_recipe_details()
+    recipeTypes = db_details[0]
+    cuisineTypes = db_details[1]
+    avoidTypes = db_details[2]  
+    return render_template('view_recipe.html', 
+        imageURL = parsedRecipe['recipe']['image_url'],
+        name = parsedRecipe['recipe']['name'],
+        ingredients = parsedRecipe['recipe']['recipeIngredients'],
+        instructions = parsedRecipe['recipe']['recipeInstructions'],
+        recipeURL = parsedRecipe['recipe']['recipe_url'],
+        recipeTypes = recipeTypes, 
+        cuisineTypes = cuisineTypes, 
+        avoidTypes = avoidTypes
+    )
+
+# Handler for marking a recipe as deleted
+@webapp.route('/delete_recipe', methods=['POST'])
+def deleteRecipe():
+    db_connection = connect_to_database()
+    deleteQuery = "UPDATE recipes SET isDeleted = 1 WHERE url = '" + request.form['url'] + "';"
+    execute_query(db_connection, deleteQuery)
+    print("Recipe deleted.")
+    return render_template('delete_successful.html')
